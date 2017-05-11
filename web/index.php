3e175af93c8a6816cc3969b464c0c15556861a07
Pierre Pic <pierre.pic63122@gmail.com>
2017-05-11 14:55:01 +0200

    Correction

--- web/index.php
+++ web/index.php
@@ -4,9 +4,7 @@ require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
-$bookservice = "https://shopping-service-p2017.herokuapp.com/book/";
-$stockservice = "";
-$wholesalerservice = "";
+
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -26,10 +24,6 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/books', function() use($app) {
-  $subRequest = Request::create($bookservice,{isbn:123456789});
-  var_dump($subRequest);
-  $book = $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
-  var_dump($book);
   return $app['twig']->render('books.twig');
 });
 
