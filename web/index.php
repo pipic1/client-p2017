b897f3ff837c65226bdd5d2ddf08a7773fa48f74
Pierre Pic <pierre.pic63122@gmail.com>
2017-05-11 14:51:02 +0200

    SubRequest gesture

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@ require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
+$bookservice = "https://shopping-service-p2017.herokuapp.com/book/";
+$stockservice = "";
+$wholesalerservice = "";
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -23,7 +26,10 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/books', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
+  $subRequest = Request::create($bookservice,{isbn:123456789});
+  var_dump($subRequest)
+  $book = $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+  var_dump($book)
   return $app['twig']->render('books.twig');
 });
 
