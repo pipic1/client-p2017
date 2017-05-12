40cefe3350340fc58c0e1c5040a7ae13a7786ddd
Pierre Pic <pierre.pic63122@gmail.com>
2017-05-12 13:23:46 +0200

    get request for find parameter

--- web/index.php
+++ web/index.php
@@ -5,40 +5,30 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-// Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
 
-$app->get('/books', function() use($app) {
+$app->get('/books', function(Request $request) use($app) {
   $app['monolog']->addDebug('logging output.');
   $url = 'https://shopping-service-p2017.herokuapp.com/book?isbn=123456789';
   $ch = curl_init();
-  // Disable SSL verification
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  // Will return the response, if false it print the response
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  // Set the url
   curl_setopt($ch, CURLOPT_URL,$url);
-  // Execute
   $result=curl_exec($ch);
-  // Closing
   curl_close($ch);
-
-  // Will dump a beauty json :3
-  var_dump(json_decode($result, true));
+  var_dump(json_decode($request, true));
   return $app['twig']->render('books.twig',array('book' => $result ));
 });
 
