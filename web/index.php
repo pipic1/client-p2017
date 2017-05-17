354adbdc3109d02490c00e460795f34940308bef
Pierre <pipic1@pedro-All-Series>
2017-05-18 00:13:22 +0200

    use provider form

--- web/index.php
+++ web/index.php
@@ -61,37 +61,39 @@ $app->get('/books', function() use($app) {
   curl_setopt($ch, CURLOPT_URL,$url);
   $result=curl_exec($ch);
   curl_close($ch);
-  return $app['twig']->render('books.twig',array('books' => json_decode($result) ));
+  return getForm($app->request);
 });
 
-$app->match('/form', function (Request $request) use ($app) {
-    // some default data for when the form is displayed the first time
-    $data = array(
-        'quantity' => 'Your quantity',
-    );
+public function getForm($request)
+{
+  $data = array(
+    'quantity' => 'Your quantity',
+  );
 
-    $form = $app['form.factory']->createBuilder(FormType::class, $data)
-        ->add('quantity')
-        ->add('submit', SubmitType::class, [
-            'label' => 'Save',
-        ])
-        ->getForm();
+  $form = $app['form.factory']->createBuilder(FormType::class, $data)
+  ->add('quantity')
+  ->add('submit', SubmitType::class, [
+    'label' => 'Save',
+  ])
+  ->getForm();
 
-    $form->handleRequest($request);
+  $form->handleRequest($request);
 
-    if ($form->isValid()) {
-        $data = $form->getData();
+  if ($form->isValid()) {
+    $data = $form->getData();
 
-        // do something with the data
+    // do something with the data
 
-        // redirect somewhere
-        var_dump("test");
-        return $app['twig']->render('book.twig',array('book' => $result));
-    }
+    // redirect somewhere
+    var_dump("test");
+    return $app['twig']->render('book.twig',array('book' => $result));
+  }
 
-    // display the form
-    return $app['twig']->render('form.twig', array('form' => $form->createView()));
-});
+  // display the form
+  return $app['twig']->render('books.twig',array('books' array('form' => $form->createView()));
+  # code...
+}
+
 /*$app->post('/buybook', function() use($app) {
   $app->get('/buybook')->request->get('name');
   $post = array(
